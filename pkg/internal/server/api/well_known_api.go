d9fe0c6789b3db7f01467c996af38ba658d0cde6
LittleSheep <littlesheep.code@hotmail.com>
2025-01-26 00:38:28 +0800

    :necktie: Stop showing disabled news source in well known

--- pkg/internal/server/api/well_known_api.go
+++ pkg/internal/server/api/well_known_api.go
@@ -1,10 +1,14 @@
 package api
 
 import (
+	"git.solsynth.dev/hypernet/reader/pkg/internal/models"
 	"git.solsynth.dev/hypernet/reader/pkg/internal/services"
 	"github.com/gofiber/fiber/v2"
+	"github.com/samber/lo"
 )
 
 func getNewsSources(c *fiber.Ctx) error {
-	return c.JSON(services.NewsSources)
+	return c.JSON(lo.Filter(services.NewsSources, func(item models.NewsSource, index int) bool {
+		return item.Enabled
+	}))
 }
